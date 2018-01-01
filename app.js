c1bd2d405258fe390910116ae9383ae9a53f8cef
CAtOSe <jasenasp@gmail.com>
2018-01-01 18:37:49 +0200

    Added PostgreSQL

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
-var app = express()
 var site = require('./routers/site');
 var gallery = require('./routers/gallery');
 var user = require('./routers/user');
+var app = express()
 app.set('view engine', 'ejs')
 
+app.use('/assets', express.static('assets'))
 app.use('/', site)
 app.use('/', gallery)
 app.use('/', user)
--- routers/user.js
+++ routers/user.js
@@ -1,12 +1,35 @@
+var args = process.argv.slice(2);
 var express = require('express')
 var router = express.Router()
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'postgres',
+  host: args[0],
+  password: args[1],
+  database: 'test',
+  port: '5432'
+})
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
 router.all('/u/:userID',(req,res)=>{
-  var user = {
-    name: 'CAtOSe',
-    id: req.params['userID']
-  }
-  res.render('user/user', {user: user})
+  pool.query('SELECT * FROM users WHERE id = $1', [req.params['userID']], (err, qres) => {
+    if (err) {
+      throw err
+    }
+
+    var user = {
+      username: qres.rows[0]['username'],
+      fullname: qres.rows[0]['fullname'],
+      id: req.params['userID']
+    }
+
+    res.render('user/user', {user: user})
+  })
 })
 
 module.exports = router
