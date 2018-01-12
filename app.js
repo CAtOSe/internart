b182e6f90f5eb037f73e535055ab86a1301b1d55
CAtOSe <jasenasp@gmail.com>
2018-01-12 21:13:34 +0200

    Reworked UserAPI and app.js relationship

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
+var args = process.argv.slice(2);
 var express = require('express')
+var passport = require('passport')
 var session = require('express-session')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var flash = require('connect-flash')
+const { Pool } = require('pg')
 
-var staticRouter = require('./routers/static');
-var site = require('./routers/site');
-var gallery = require('./routers/gallery');
-var user = require('./routers/user');
+var staticRouter = require('./routers/static')
+var site = require('./routers/site')
+var gallery = require('./routers/gallery')
+
 
 var app = express()
 app.set('view engine', 'ejs')
+app.use(cookieParser());
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }))
 
+
+app.use(session({ secret: 'iloveapple' }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+
+const pool = new Pool({
+  user: args[1],
+  host: args[0],
+  password: args[2],
+  database: 'internart',
+  port: '5432'
+})
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
+require('./routers/user')(app, passport, pool)
+
 app.use('/assets', express.static('assets'))
 app.use('/', staticRouter)
 app.use('/', site)
 app.use('/', gallery)
-app.use('/', user)
 
 app.all('/api/*', (req, res) => {
   var response={
--- routers/user.js
+++ routers/user.js
@@ -1,96 +1,145 @@
-var args = process.argv.slice(2);
-var express = require('express')
-var randomstring = require('randomstring')
-var passport = require('passport')
-var session = require('express-session')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-var flash = require('connect-flash')
-var router = express.Router()
+module.exports = function(app, passport, pool){
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  user: args[1],
-  host: args[0],
-  password: args[2],
-  database: 'internart',
-  port: '5432'
-})
-
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-})
-
-
-router.post('/api/u/getUser', (req, res) => {
-  if(req.body.userID == undefined){
-    var response = {
-      status: {
-        code: 400,
-        message: "UserID undefined"
-      }
-    }
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(response))
-    return
-  }
-
-  pool.query('SELECT id, username, fullname, access FROM users WHERE id = $1', [req.body.userID], (err, qres) => {
-    if (err) {
+  app.post('/api/u/getUser', (req, res) => {
+    if(req.body.userID == undefined){
       var response = {
         status: {
-          code: 500,
-          message: "SQL error"
-        },
-        error: err
+          code: 400,
+          message: "UserID undefined"
+        }
       }
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify(response))
-      throw err
       return
-    }else if(qres.rows.length>0){
-      var response = {
-        status: {
-          code: 200,
-          message: "User found"
-        },
-        data: qres.rows[0]
-      }
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(response))
-    }else{
+    }
+
+    pool.query('SELECT id, username, fullname, access FROM users WHERE id = $1', [req.body.userID], (err, qres) => {
+      if (err) {
         var response = {
           status: {
-            code: 204,
-            message: "User not found"
-          }
+            code: 500,
+            message: "SQL error"
+          },
+          error: err
         }
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(response))
+        throw err
+        return
+      }else if(qres.rows.length>0){
+        var response = {
+          status: {
+            code: 200,
+            message: "User found"
+          },
+          data: qres.rows[0]
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(response))
+      }else{
+          var response = {
+            status: {
+              code: 204,
+              message: "User not found"
+            }
+          }
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(response))
+      }
+    })
+  })
+
+
+
+  app.post('/api/u/createUser', (req, res) => {
+    if(req.body.userData == undefined){
+      var response = {
+        status: {
+          code: 400,
+          message: "user data undefined"
+        }
+      }
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify(response))
+      return
     }
+
+    function generateID() {
+      var id = randomstring.generate(4)
+      pool.query('SELECT id FROM users WHERE id = $1', [id], (err, qres) => {
+        if(err) {
+          var response = {
+            status: {
+              code: 500,
+              message: "SQL error"
+            }
+          }
+          res.setHeader('Content-Type', 'application/json')
+          res.send(JSON.stringify(response))
+          return
+        }else if(qres){
+          var userData = JSON.parse(req.body.userData)
+          pool.query('INSERT INTO users(id, username, fullname, password, access) VALUES ($1, $2, $3, $4, $5)', [id, userData['username'], userData['fullname'], userData['password'], userData['access']], (err, qres) => {
+            if (err){
+              if (err['detail'].includes('already exists')){
+                var response = {
+                  status: {
+                    code: 400,
+                    message: "User already exists"
+                  }
+                }
+                res.setHeader('Content-Type', 'application/json')
+                res.send(JSON.stringify(response))
+
+              }else{
+                var response = {
+                  status: {
+                    code: 500,
+                    message: "SQL error"
+                  }
+                }
+                res.setHeader('Content-Type', 'application/json')
+                res.send(JSON.stringify(response))
+              }
+            }else{
+              var response = {
+                status: {
+                  code: 201,
+                  message: "User created"
+                },
+                data: {userID: id}
+              }
+              res.setHeader('Content-Type', 'application/json')
+              res.send(JSON.stringify(response))
+
+            }
+          })
+        }else{
+          generateID()
+        }
+      })
+    }
+
+    generateID()
   })
-})
 
 
 
-router.post('/api/u/createUser', (req, res) => {
-  if(req.body.userData == undefined){
-    var response = {
-      status: {
-        code: 400,
-        message: "user data undefined"
+
+  app.post('/api/u/deleteUser', (req, res) => {
+    if(req.body.userID == undefined){
+      var response = {
+        status: {
+          code: 400,
+          message: "userID undefined"
+        }
       }
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(response))
+      return
     }
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(response))
-    return
-  }
-
-  function generateID() {
-    var id = randomstring.generate(4)
-    pool.query('SELECT id FROM users WHERE id = $1', [id], (err, qres) => {
-      if(err) {
+    pool.query('DELETE FROM users WHERE id = $1', [req.body.userID], (err, qres) => {
+      if (err){
         var response = {
           status: {
             code: 500,
@@ -99,104 +148,34 @@ router.post('/api/u/createUser', (req, res) => {
         }
         res.setHeader('Content-Type', 'application/json')
         res.send(JSON.stringify(response))
-        return
-      }else if(qres){
-        var userData = JSON.parse(req.body.userData)
-        pool.query('INSERT INTO users(id, username, fullname, password, access) VALUES ($1, $2, $3, $4, $5)', [id, userData['username'], userData['fullname'], userData['password'], userData['access']], (err, qres) => {
-          if (err){
-            if (err['detail'].includes('already exists')){
-              var response = {
-                status: {
-                  code: 400,
-                  message: "User already exists"
-                }
-              }
-              res.setHeader('Content-Type', 'application/json')
-              res.send(JSON.stringify(response))
-
-            }else{
-              var response = {
-                status: {
-                  code: 500,
-                  message: "SQL error"
-                }
-              }
-              res.setHeader('Content-Type', 'application/json')
-              res.send(JSON.stringify(response))
-            }
-          }else{
-            var response = {
-              status: {
-                code: 201,
-                message: "User created"
-              },
-              data: {userID: id}
-            }
-            res.setHeader('Content-Type', 'application/json')
-            res.send(JSON.stringify(response))
-
+      } else if (qres.rowCount == 0){
+        var response = {
+          status: {
+            code: 204,
+            message: "User not found"
           }
-        })
-      }else{
-        generateID()
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(response))
+      }else if (qres.rowCount > 0){
+        var response = {
+          status: {
+            code: 200,
+            message: "User deleted"
+          }
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(response))
       }
     })
-  }
-
-  generateID()
-})
+  })
 
 
 
+  app.post('u/login', (req, res) => {
+    if(req.body.loginName == undefined || req.body.loginPassword == undefined){
 
-router.post('/api/u/deleteUser', (req, res) => {
-  if(req.body.userID == undefined){
-    var response = {
-      status: {
-        code: 400,
-        message: "userID undefined"
-      }
-    }
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(response))
-    return
-  }
-  pool.query('DELETE FROM users WHERE id = $1', [req.body.userID], (err, qres) => {
-    if (err){
-      var response = {
-        status: {
-          code: 500,
-          message: "SQL error"
-        }
-      }
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(response))
-    } else if (qres.rowCount == 0){
-      var response = {
-        status: {
-          code: 204,
-          message: "User not found"
-        }
-      }
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(response))
-    }else if (qres.rowCount > 0){
-      var response = {
-        status: {
-          code: 200,
-          message: "User deleted"
-        }
-      }
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(response))
     }
   })
-})
 
-router.post('u/login', (req, res) => {
-  if(req.body.loginName == undefined || req.body.loginPassword == undefined){
-
-  }
-})
-
-module.exports = router
+}
